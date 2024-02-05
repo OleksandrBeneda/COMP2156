3fbb14d32d7491b981675a2fa3ce78541a6eec10
Oleksandr <156257721+OleksandrBeneda@users.noreply.github.com>
2024-02-04 20:16:30 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Xml.Linq;
+using System;
+
+name: DevOps Lab Exercises
+on:
+  push:
+branches:
+-main
+jobs:
+build:
+runs - on: ubuntu - latest
+    steps:
+-name: Checkout code
+        uses: actions / checkout@v4
+      - name: Use Node.js
+        uses: actions/setup-node @v3
+        with:
+          node-version: '20.x'
+      - run: npm ci
+      - run: npm run build --if-present
+      - run: npm test
